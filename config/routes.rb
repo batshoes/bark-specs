ef1271fbba9153ba4da67bcad46a694d517c12d1
James Middlemiss <middlemissj.usa@gmail.com>
2015-07-30 13:41:45 -0400

    Fixed first of 16 specs

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get "/log-in" => "sessions#new", as: :log_in
   post "/log-in" => "sessions#create"
   delete "/log-out" => "sessions#destroy", as: :log_out
+  #get '/statuses' => "statuses#index", as: :statuses
+
+  resources :statuses
+
   resources :dogs do
     resources :statuses
   end
